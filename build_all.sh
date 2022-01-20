52c91a441dcb88578dcd05ec6fb3ab45bb6d07a7
fanchao <git@fanchao.nz>
2022-01-20 22:15:55 +1300

    Build script

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in x86_64-unknown-linux-gnu; do
+for i in x86_64-unknown-linux-gnu aarch64-unknown-linux-gnu; do
   rustup target add $i
-  cargo build --release --target $i && mv -v target/$i/release/proxy proxy-$i || exit 1
+  cross build --release --target $i && mv -v target/$i/release/proxy proxy-$i || exit 1
 done
