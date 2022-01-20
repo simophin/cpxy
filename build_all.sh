099795795f058d029f2816f5f55d57d2878339c1
simophin <dev@fanchao.nz>
2022-01-20 22:49:04 +1300

    Targets and placement

--- build_all.sh
+++ build_all.sh
@@ -2,7 +2,8 @@
 
 cargo install cross
 
-for i in aarch64-unknown-linux-gnu; do
+for i in aarch64-unknown-linux-gnu x86_64-apple-darwin x86_64-unknown-linux-gnu aarch64-apple-darwin; do
   rustup target add $i
-  cross build --release --target $i && mv -v target/$i/release/proxy proxy-$i || exit 1
+  mkdir -p bin/$i
+  cross build --release --target $i && mv -v target/$i/release/proxy bin/$i/ || exit 1
 done
