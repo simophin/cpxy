5b8b4344013663e117998b7df24299d1664f87e7
simophin <dev@fanchao.nz>
2022-01-21 08:16:22 +1300

    New build script

--- build_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cargo install cross
-
-for i in aarch64-unknown-linux-gnu x86_64-apple-darwin x86_64-unknown-linux-gnu aarch64-apple-darwin; do
-  rustup target add $i
-  mkdir -p bin/$i
-  cross build --release --target $i && mv -v target/$i/release/proxy bin/$i/ || exit 1
-done
