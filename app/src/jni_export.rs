2a716140aaf5c4cf4b89b3337cb9dd01638574dd
simophin <dev@fanchao.nz>
2022-02-07 18:49:03 +1300

    Added getPort

--- app/src/jni_export.rs
+++ app/src/jni_export.rs
@@ -1,6 +1,6 @@
 use crate::controller::run_controller;
 use jni::objects::{JClass, JString};
-use jni::sys::jlong;
+use jni::sys::{jint, jlong};
 use jni::JNIEnv;
 use smol::{spawn, Async, Task};
 use std::path::Path;
@@ -62,6 +62,18 @@ pub extern "system" fn Java_dev_fanchao_CJKProxy_start(
         port,
         spawn(async move { run_controller(listener, Path::new(&config_path)).await }),
     ))) as *mut Instance as jlong
+}
+
+#[no_mangle]
+pub extern "system" fn Java_dev_fanchao_CJKProxy_getPort(
+    _: JNIEnv,
+    _: JClass,
+    instance: jlong,
+) -> jint {
+    let handle: Box<Instance> = unsafe { Box::from_raw(instance as *mut Instance) };
+    let result = handle.0;
+    let _ = Box::leak(handle);
+    result as jint
 }
 
 #[no_mangle]
