79dc462d23d7301316688c96c2fef2ea7ec29548
simophin <dev@fanchao.nz>
2022-02-08 16:54:04 +1300

    Build script

---
+++ build_linux.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+cargo install cross
+
+configs=(
+  x86_64-unknown-linux-gnu
+  aarch64-unknown-linux-gnu
+  armv7-unknown-linux-gnueabihf
+  mips-unknown-linux-musl 
+)
+
+for ((i=0; i<${#configs[@]}; )); do
+  target=${configs[i++]}
+  echo Buildling $target
+  cross build --package cjk-proxy --release --target=$target
+done
--- build_unix.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-cargo install cross
-
-for i in "$@"; do
-  echo Buildling $i
-  cross build --package cjk-proxy --release --target=$i
-done
