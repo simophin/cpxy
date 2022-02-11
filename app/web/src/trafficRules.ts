96430f0677d14ba5097a56241414d2e268c9a6ac
simophin <dev@fanchao.nz>
2022-02-11 17:08:32 +1300

    Fixed rule matching

--- app/web/src/trafficRules.ts
+++ app/web/src/trafficRules.ts
@@ -1,9 +1,14 @@
 import _ from "lodash";
 
-const GEOIP_PAT = /^geoip:[a-z]{2}$/i;
-const GFWLIST_PAT = /^gfwlist$/i;
-const NETWORK_PAT = /^network:.+?$/i;
-const DOMAIN_PAT = /^domain:.+?$/i;
+
+
+const PATTERNS = [
+    /^geoip:[a-z]{2}$/i,
+    /^gfwlist$/i,
+    /^network:.+?$/i,
+    /^domain:.+?$/i,
+    /^adblocklist$/i,
+]
 
 export function transformRule(value: string): string[] {
     const separator = value.indexOf("\r\n") >= 0 ? "\r\n" : "\n";
@@ -12,10 +17,13 @@ export function transformRule(value: string): string[] {
         if (trimmed.length === 0) {
             return undefined;
         }
-        if (trimmed.match(GEOIP_PAT) || trimmed.match(GFWLIST_PAT) || trimmed.match(NETWORK_PAT) || trimmed.match(DOMAIN_PAT)) {
-            return trimmed;
-        } else {
-            throw new Error(`Rule ${trimmed} is invalid`);
+
+        for (const pat in PATTERNS) {
+            if (trimmed.match(pat)) {
+                return trimmed;
+            }
         }
+
+        throw new Error(`Rule ${trimmed} is invalid`);
     }).filter((s) => !!s).map((s) => s ?? '');
 }
