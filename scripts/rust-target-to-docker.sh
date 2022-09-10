9a277c2e9e7519e6ecbb9b2e593204b2fba6d38a
fanchao <dev@fanchao.nz>
2022-09-10 15:09:06 +1200

    Fixed arm64 docker image

--- scripts/rust-target-to-docker.sh
+++ scripts/rust-target-to-docker.sh
@@ -3,7 +3,7 @@
 if [[ $1 == x86_64*linux* ]]; then
     echo amd64
 elif [[ $1 == aarch64*linux* ]]; then
-    echo arm64
+    echo arm64v8
 elif [[ $1 == armv7*linux* ]]; then
     echo arm32v7
 elif [[ $1 == i686*linux* ]]; then
